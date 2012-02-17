15b7a5c62e9bce1874099c819078cebe9d31b5fb
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2012-02-17 23:33:05 +0100

    Fixed views

---
+++ haskell/Dope/View/PlayerIntrospection.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Dope.View.PlayerIntrospection where
+
+import qualified Dope.Model.Player as Player
+import Dope.Model.Player (Player (Player))
+
+import Dope.Model.DeriveJson
+import Data.Label
+
+-- TODO: This should have its own fields, but I'm too lazy
+data PlayerIntrospection = PlayerIntrospection {
+    _player :: Player
+    }
+$(mkLabels [''PlayerIntrospection])
+$(derive makeJSON ''PlayerIntrospection)
+
+fromPlayer :: Player -> PlayerIntrospection
+fromPlayer player = PlayerIntrospection player
+
